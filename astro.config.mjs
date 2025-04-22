d0410637be116a3bc1726ad88c295eeb57365a53
ouariachi <mmohamedelouariachi@gmail.com>
2025-04-22 18:12:12 +0200

    ✨ feat: implement header component with theme toggle and localization support
    - add social media icons for Github, Linkedin, Mail, Spain, United Kingdom, and X
    - remove Welcome component and update layout structure
    - add global styles for theming and custom cursor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+    routing: "manual"
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
