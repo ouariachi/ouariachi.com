78802688ccca0d3f8a02dc70679bbcb49f6a086a
ouariachi <mmohamedelouariachi@gmail.com>
2025-04-22 20:15:05 +0200

    ✨ feat(routing): removed redirect from middleware and handle default locale logic in index.astro

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,7 @@
 import { defineMiddleware } from 'astro/middleware';
 import { redirectToDefaultLocale, pathHasLocale } from 'astro:i18n';
 
+// i18n manual routing requires a middleware file.
 export const onRequest = defineMiddleware((ctx, next) => {
-  const { pathname } = ctx.url;
-  if (pathHasLocale(pathname)) {
-    return next();
-  } else {
-    return redirectToDefaultLocale(ctx, 301);
-  }
+  return next();
 });
