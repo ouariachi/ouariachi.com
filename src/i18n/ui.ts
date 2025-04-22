e74cb87c5ea9e057c17d29f3ccf5b9298eb434e6
ouariachi <mmohamedelouariachi@gmail.com>
2025-04-22 19:17:10 +0200

    ✨ feat: add navigation links

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,19 +7,21 @@ export const languages = {
 
 export const ui = {
   es: {
-    "about": "Sobre mí",
-    "contact": "Contacto",
-    "blog": "Blog",
+    "nav.about": "Sobre mí",
+    "nav.contact": "Contacto",
+    "nav.blog": "Blog",
     "home.occupation": "Ingeniero de Software",
     "home.changeLanguage": "Cambiar idioma",
     "home.imageAlt": "Mohamed en estilo de cartoon",
+    "home.about": "Desarrollador enfocado en crear soluciones eficientes y escalables. Me apasiona el código limpio y construir herramientas que faciliten el trabajo."
   },
   en: {
-    "about": "About me",
-    "contact": "Contact",
-    "blog": "Blog",
+    "nav.about": "About me",
+    "nav.contact": "Contact",
+    "nav.blog": "Blog",
     "home.occupation": "Software Engineer",
     "home.changeLanguage": "Change language",
     "home.imageAlt": "Mohamed in cartoon style",
+    "home.about": "Developer focused on creating efficient, scalable solutions. I’m passionate about clean code and building tools that make life easier."
   }
 };
