f3cd82a8eeeec7a3b84809a1182c82b92b83cd2b
ouariachi <mmohamedelouariachi@gmail.com>
2025-06-28 16:50:48 +0200

    ✨ feat(config): update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ouariachi.com",
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
     routing: "manual"
   },
   vite: {
+    base: "/",
     plugins: [tailwindcss()],
     server: {
       allowedHosts: ['ouariachi.com', 'www.ouariachi.com']
